442e43aaedb5b3cc345807c74780afbef3be9889
SaadRahim7 <saadrahim.201@gmail.com>
2024-10-18 17:43:05 +0500

    changes

--- lib/provider/api_provider.dart
+++ lib/provider/api_provider.dart
@@ -68,33 +68,33 @@ class ApiProvider with ChangeNotifier {
     return watchlist;
   }
 
-  Future<List<ProfitLoss>> profitloss(String username, String mode) async {
-    var queryParameters = {
-      'user_id': username,
-      'selected_environment': mode,
-    };
+  // Future<List<ProfitLoss>> profitloss(String username, String mode) async {
+  //   var queryParameters = {
+  //     'user_id': username,
+  //     'selected_environment': mode,
+  //   };
 
-    Request req = Request(
-        'GET', Uri.parse('https://www.tradergpt.co/api/profit_and_loss'))
-      ..body = json.encode(queryParameters)
-      ..headers.addAll({
-        "Content-type": "application/json",
-      });
-    var response = await req.send();
+  //   Request req = Request(
+  //       'GET', Uri.parse('https://www.tradergpt.co/api/profit_and_loss'))
+  //     ..body = json.encode(queryParameters)
+  //     ..headers.addAll({
+  //       "Content-type": "application/json",
+  //     });
+  //   var response = await req.send();
 
-    // Convert the streamed response to string
-    var responseString = await response.stream.bytesToString();
+  //   // Convert the streamed response to string
+  //   var responseString = await response.stream.bytesToString();
 
-    // Parse the string as JSON (if the response is in JSON format)
-    final List<dynamic> jsonData = jsonDecode(responseString);
-    List<ProfitLoss> profitloss =
-        jsonData.map<ProfitLoss>((json) => ProfitLoss.fromJson(json)).toList();
+  //   // Parse the string as JSON (if the response is in JSON format)
+  //   final List<dynamic> jsonData = jsonDecode(responseString);
+  //   List<ProfitLoss> profitloss =
+  //       jsonData.map<ProfitLoss>((json) => ProfitLoss.fromJson(json)).toList();
 
-    // Do something with the jsonData
-    logger.i(jsonData);
+  //   // Do something with the jsonData
+  //   logger.i(jsonData);
 
-    return profitloss;
-  }
+  //   return profitloss;
+  // }
 
   Future<List<Order>> getOrders(String username, String mode) async {
     var queryParameters = {
@@ -175,5 +175,53 @@ class ApiProvider with ChangeNotifier {
     logger.i(jsonData);
 
     return communitystrategiesResponse;
+  }
+
+  Future<String> getstrategiecode(String username, String strategy) async {
+    var queryParameters = {
+      'user_id': username,
+      'strategy_id': strategy,
+    };
+
+    Request req =
+        Request('GET', Uri.parse('https://www.tradergpt.co/api/strategy/code'))
+          ..body = json.encode(queryParameters)
+          ..headers.addAll({
+            "Content-type": "application/json",
+          });
+    var response = await req.send();
+
+    // Convert the streamed response to string
+    var responseString = await response.stream.bytesToString();
+
+    logger.i(responseString);
+
+    return responseString;
+  }
+
+  Future<ProfitLoss> getProfitLoss(String username, String mode) async {
+    var queryParameters = {
+      'user_id': username,
+      'selected_environment': mode,
+    };
+
+    Request req = Request(
+        'GET', Uri.parse('https://www.tradergpt.co/api/profit_and_loss'))
+      ..body = json.encode(queryParameters)
+      ..headers.addAll({
+        "Content-type": "application/json",
+      });
+    var response = await req.send();
+
+    // Convert the streamed response to string
+    var responseString = await response.stream.bytesToString();
+
+    final Map<String, dynamic> jsonData = jsonDecode(responseString);
+
+    ProfitLoss data = ProfitLoss.fromJson(jsonData);
+
+    logger.i(jsonData);
+
+    return data;
   }
 }
---
+++ lib/provider/strategie_code_provider.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/provider/api_provider.dart';
+
+class StrategiesCodeProvider with ChangeNotifier {
+  String? strategieCode;
+  bool isLoading = false;
+  String? errorMessage;
+
+
+
+  Future<void> fetchStratrgieCode(String username, String strategieid) async {
+    isLoading = true;
+    errorMessage = null;
+    notifyListeners();
+
+    try {
+      strategieCode =
+          await ApiProvider().getstrategiecode(username, strategieid);
+    } catch (error) {
+      errorMessage = 'Failed to load data: $error';
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+}
